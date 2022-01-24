540a0def19cf77f2c6f2200aff7a53040092cdf1
Jake Serwin <88554771+jkserwin@users.noreply.github.com>
2022-01-24 15:19:14 -0800

    updates

--- routes/index.js
+++ routes/index.js
@@ -78,7 +78,7 @@ router.post('/books/:id', asyncHandler(async (req, res) => {
 }));
 
 /* POST delete book listing. */
-router.post('/books/:id', asyncHandler(async (req, res) => {
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
     await book.destroy();
